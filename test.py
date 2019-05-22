d273029661605e781520bc35ad808317a27f5d74
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2019-05-22 15:20:08 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+'''
+Author: Osman Çakmak
+Skype: oxcakmak
+Email: oxcakmak@hotmail.com
+Website: http://oxcakmak.com/
+Country: Turkey [TR]
+'''
+def Reverse(lst):
+    return [ele for ele in reversed(lst)]
+    
+lst = [11, 12, 13, 14, 15]
+
+print(lst)
+print(Reverse(lst))
